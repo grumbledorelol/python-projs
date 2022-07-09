b9d50b5ca30ad3fab9aa6b8c11186423811bfa93
grumbledorelol <68572255+grumbledorelol@users.noreply.github.com>
2022-07-08 22:24:15 -0700

    Update passgenerator.py

--- passgenerator.py
+++ passgenerator.py
@@ -5,9 +5,6 @@ lst = []
 e = int(input("password length?"))
 n = random.randint(1,e)
 for i in range(n):
-    #lst.append(random.choice(string.ascii_letters)[0])
-    #lst.append(random.choice(string.ascii_letters)[0])
-    #lst.append(random.choice(string.printable)[0])
     lst.append(random.choice(string.printable)[0])
     
 print("".join(lst))
